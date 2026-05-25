86e8fb0d38526f14b653acee3b8bc4076872aa9c
AswathiVipin <aswathivipi2016@gmail.com>
2026-05-25 11:49:29 +0530

    first commit

--- shopproject/store/admin.py
+++ shopproject/store/admin.py
@@ -4,4 +4,8 @@ from .models import Category, Product
 
 admin.site.register(Category)
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'price', 'category', 'created_at']
+    readonly_fields =  ['created_at']
+    
